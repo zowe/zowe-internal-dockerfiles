68049a95b8b34893d48f20365be91a7f8d7a7e9d
Vitek Vlcek <vitezslavvit.vlcek@broadcom.com>
2019-10-29 18:03:00 +0100

    first zowe 1.6.0
    Signed-off-by: Vitek Vlcek <vitezslavvit.vlcek@broadcom.com>

--- dockerfiles/zowe-1.6.0/run.sh
+++ dockerfiles/zowe-1.6.0/run.sh
@@ -14,7 +14,12 @@ export ZOWE_ZOSMF_HOST=$ZOWE_ZOSMF_HOST
 export ZOWE_ZOSMF_PORT=$ZOWE_ZOSMF_PORT
 
 env
-
+echo "Installation root $ZOWE_INSTALL_ROOT" 
+if [ "$1" = "--only-install" ]; then
+    echo "Only the installation is finished, type exit to proceed to configuration"
+    cd $ZOWE_INSTALL_ROOT
+    bash
+fi
 sed -i 's/gatewayPort=7554/gatewayPort=60004/' $ZOWE_INSTALL_ROOT/scripts/configure/zowe-install.yaml
 sed -i "s/externalCertificate=/externalCertificate=$(echo "$CERTS_DIR/server.p12" |  sed 's/\//\\\//g')/" $ZOWE_INSTALL_ROOT/scripts/configure/zowe-install.yaml
 sed -i 's/externalCertificateAlias=/externalCertificateAlias=apiml/' $ZOWE_INSTALL_ROOT/scripts/configure/zowe-install.yaml
@@ -28,3 +33,14 @@ keytool -importcert -trustcacerts -alias ZOSMF -keyalg RSA -keystore $ZOWE_INSTA
 sed 's|//.*||' $ZOWE_INSTALL_ROOT/zlux-app-server/deploy/instance/ZLUX/serverConfig/zluxserver.json | jq ".agent.host=\"$ZOWE_ZSS_HOST\"" | jq ".agent.http.port=$ZOWE_ZSS_PORT" | sponge $ZOWE_INSTALL_ROOT/zlux-app-server/deploy/instance/ZLUX/serverConfig/zluxserver.json
 sed -i 's/LAUNCH_COMPONENT_GROUPS=.*//' $ZOWE_INSTALL_ROOT/scripts/internal/run-zowe.sh
 find $ZOWE_INSTALL_ROOT/ -type f -name '*.sh' -exec sh -c "chmod +x {}" \;
+
+if [ "$1" = "--only-config" ]; then
+    echo "Zowe install directory: $ZOWE_INSTALL_ROOT"
+    echo "Only the installation and configuration is finished, type exit to proceed to zowe-run.sh"
+    cd $ZOWE_INSTALL_ROOT
+    bash
+fi
+
+cd $ZOWE_INSTALL_ROOT/scripts/internal/
+bash ./run-zowe.sh
+sleep infinity
