531c165886b155416f0a534df811913ec4877c97
MarkAckert <mark.ackert@broadcom.com>
2020-02-04 13:55:43 -0500

    remove docker build from build.sh
    Signed-off-by: MarkAckert <mark.ackert@broadcom.com>

--- dockerfiles/license-scanning/build.sh
+++ dockerfiles/license-scanning/build.sh
@@ -3,5 +3,3 @@ mkdir -p build/ && cd build && rm -rf zowe-dependency-scan-pipeline && git clone
 #!/bin/bash
 rsync -av --progress build/zowe-dependency-scan-pipeline/dependency-scan include/ --delete --exclude .git --exclude build --exclude lib --exclude node_modules --exclude yarn.lock
 rsync -av --progress build/zowe-dependency-scan-pipeline/LicenseFinder include/ --delete --exclude .git --exclude ci
-
-docker build -t zowehub/zowe-dependency-scanning:latest .
