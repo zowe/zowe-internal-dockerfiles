66cdd67040231e238ba3939cce6b4ed04c6c5c5b
Timothy Gerstel <tgerstel@rocketsoftware.com>
2020-06-22 13:02:27 -0400

    Update variable name, add comments
    Signed-off-by: Timothy Gerstel <tgerstel@rocketsoftware.com>

--- dockerfiles/zowe-release/zowe-1.11.0/copy_jwt_to_zss.sh
+++ dockerfiles/zowe-release/zowe-1.11.0/copy_jwt_to_zss.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Example usage:
+# sudo ./copy_jwt_to_zss.sh -u TS6330 -rd /u/ts6330/dkr_tmp -c exciting_bhaskara
+# TODO: add comment for flags
 TEMP_DIR=/tmp/
 REMOTE_DIR=/tmp/
 OUT_FILE_NAME=jwtsecret.p12
@@ -32,14 +35,14 @@ while [ "$1" != "" ]; do
   shift
 done
 
-FULL_OUT_PATH="${TEMP_DIR}${OUT_FILE_NAME}"
-echo "OUT PATH=${FULL_OUT_PATH}"
-docker cp $CONTAINER_NAME:/global/zowe/keystore/localhost/localhost.keystore.jwtsecret.p12 $FULL_OUT_PATH
+FULL_LOCAL_OUT_PATH="${TEMP_DIR}${OUT_FILE_NAME}"
+echo "OUT PATH=${FULL_LOCAL_OUT_PATH}"
+docker cp $CONTAINER_NAME:/global/zowe/keystore/localhost/localhost.keystore.jwtsecret.p12 $FULL_LOCAL_OUT_PATH
 if [ -z "$ZSS_HOST" ]; then
   ZSS_HOST=$(docker exec $CONTAINER_NAME bash -c 'echo $ZOWE_ZSS_HOST')
 fi
 echo "Initiating scp file transfer to ${ZSS_USER}@${ZSS_HOST}"  
-if ! scp $FULL_OUT_PATH $ZSS_USER@$ZSS_HOST:$REMOTE_DIR/$OUT_FILE_NAME ; then
+if ! scp $FULL_LOCAL_OUT_PATH $ZSS_USER@$ZSS_HOST:$REMOTE_DIR/$OUT_FILE_NAME ; then
   echo "Unable to write ${ZSS_HOST}:${REMOTE_DIR}/${OUT_FILE_NAME}. File may already exist or user may have insufficient write permissions."
 fi
-rm -f $FULL_OUT_PATH
+rm -f $FULL_LOCAL_OUT_PATH
