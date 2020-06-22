d2baa06f09b5ef86184279c444fd9ef85cbb85f9
Suneeth Keerthy <skeerthy@waldevgizaud001.dev.rocketsoftware.com>
2020-06-22 15:26:25 -0400

    Clear some stuff
    Signed-off-by: Suneeth Keerthy <skeerthy@waldevgizaud001.dev.rocketsoftware.com>

--- dockerfiles/zowe-release/zowe-1.11.0/run.sh
+++ dockerfiles/zowe-release/zowe-1.11.0/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sed -i 's/-ebcdic//' /global/zowe/keystore/zowe-certificates.env
-#sed -i -e 's/ZOWE_EXPLORER_FRAME_ANCESTORS="${ZOWE_EXPLORER_HOST}:*,${ZOWE_IP_ADDRESS}:*"/ZOWE_EXPLORER_FRAME_ANCESTORS="${ZOWE_EXPLORER_HOST}:*,${ZOWE_IP_ADDRESS}:*,${LINUX_HOST}:*"/g' /root/zowe/instance/instance.env
 bash /root/zowe/instance/bin/internal/run-zowe.sh
 sleep infinity
 
