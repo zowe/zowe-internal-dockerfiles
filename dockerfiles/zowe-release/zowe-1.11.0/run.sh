173b210bc8b0981798fd89157257b3a6e889e586
Timothy Gerstel <tgerstel@rocketsoftware.com>
2020-06-23 17:41:11 -0400

    Read env var for launch component groups
    Signed-off-by: Timothy Gerstel <tgerstel@rocketsoftware.com>

--- dockerfiles/zowe-release/zowe-1.11.0/run.sh
+++ dockerfiles/zowe-release/zowe-1.11.0/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sed -i 's/LAUNCH_COMPONENT_GROUPS=.*/LAUNCH_COMPONENT_GROUPS='"${LAUNCH_COMPONENT_GROUPS}"'/g' /root/zowe/instance/instance.env
 bash /root/zowe/instance/bin/internal/run-zowe.sh
 sleep infinity
 
