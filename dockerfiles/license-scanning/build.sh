27de99d59af873f770a876479006c431ea376d09
MarkAckert <mark.ackert@broadcom.com>
2021-05-13 17:12:47 -0400

    update license_finder build script
    Signed-off-by: MarkAckert <mark.ackert@broadcom.com>

--- dockerfiles/license-scanning/build.sh
+++ dockerfiles/license-scanning/build.sh
@@ -1,5 +1,5 @@
 
 mkdir -p build/ && cd build && rm -rf zowe-dependency-scan-pipeline && git clone https://github.com/zowe/zowe-dependency-scan-pipeline && cd ..
 #!/bin/bash
-rsync -av --progress build/zowe-dependency-scan-pipeline/dependency-scan include/ --delete --exclude .git --exclude build --exclude lib --exclude node_modules --exclude yarn.lock
-rsync -av --progress build/zowe-dependency-scan-pipeline/LicenseFinder include/ --delete --exclude .git --exclude ci
+rsync -av --progress build/zowe-dependency-scan-pipeline/licenses/dependency-scan include/ --delete --exclude .git --exclude build --exclude lib --exclude node_modules --exclude yarn.lock
+rsync -av --progress build/zowe-dependency-scan-pipeline/licenses/LicenseFinder include/ --delete --exclude .git --exclude ci
