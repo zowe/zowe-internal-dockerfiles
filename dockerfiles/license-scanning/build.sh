f25221d87f1a579766f8dd31d7ae0b4213e3f4ca
MarkAckert <mark.ackert@broadcom.com>
2021-05-14 11:01:20 -0400

    add static resources to license scanning image
    Signed-off-by: MarkAckert <mark.ackert@broadcom.com>

--- dockerfiles/license-scanning/build.sh
+++ dockerfiles/license-scanning/build.sh
@@ -3,3 +3,4 @@ mkdir -p build/ && cd build && rm -rf zowe-dependency-scan-pipeline && git clone
 #!/bin/bash
 rsync -av --progress build/zowe-dependency-scan-pipeline/licenses/dependency-scan include/ --delete --exclude .git --exclude build --exclude lib --exclude node_modules --exclude yarn.lock
 rsync -av --progress build/zowe-dependency-scan-pipeline/licenses/LicenseFinder include/ --delete --exclude .git --exclude ci
+rsync -av --progress build/zowe-dependency-scan-pipeline/licenses/resources include/ --delete --exclude .git --exclude ci
